08ba87cb666854c2dec6a7a0b94314c8ecadc60d
Irdan Guntara <67906872+irza1@users.noreply.github.com>
2026-03-08 11:01:55 +0700

    Force HTTPS asset URLs on Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+
+            $appUrl = config('app.url');
+
+            if (is_string($appUrl) && str_starts_with($appUrl, 'https://')) {
+                URL::forceRootUrl($appUrl);
+            }
+        }
     }
 }
